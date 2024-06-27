65cfc7192c7a62ec8267a46c63f93720a45dd560
Dani <daniho96@gmail.com>
2024-06-26 23:54:06 -0500

    update config.js to accept spec.js pattern

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    // Base URL for the tests
+    baseUrl: 'https://www.zillow.com',
+    specPattern: 'cypress/e2e/**/*.spec.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
